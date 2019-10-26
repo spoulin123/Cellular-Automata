6bd23d35c92f03bc65164ef97fbd91c0c43735bf
Hykilpikonna <admin@moecraft.cc>
2019-10-26 14:40:35 -0400

    [+] Create func to draw vertical gridlines

--- src/renderer.py
+++ src/renderer.py
@@ -16,3 +16,19 @@ class Renderer:
         self.pygame = pygame
         self.window = window
 
+    # Draw grid lines
+    def draw_lines(self):
+
+        # Loop through all x values in the grid
+        for x in range(0, self.window.get_width()):
+
+            # Every gap between blocks
+            if x % block_width == 0:
+                print('x = ' + str(x))
+
+                # Draw a line
+                self.pygame.draw.rect(self.window, grid_line_color, (x, 0, grid_line_width, self.window.get_height()))
+
+
+
+
