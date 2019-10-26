1bc71ba1516572ea2ef2dc06dccf0601997ab498
Hykilpikonna <admin@moecraft.cc>
2019-10-26 14:10:01 -0400

    [+] Monitor pygame events

--- src/graphics.py
+++ src/graphics.py
@@ -5,3 +5,14 @@ pygame.init()
 
 # Create pygame window
 window = pygame.display.set_mode((1000, 1000))
+
+# Monitor events
+running = True
+while running:
+    # Delay
+    pygame.time.delay(10)
+
+    # Loop through events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
