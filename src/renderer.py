02d5279319ebb7d4fc2d33448a4780da49b35df7
Hykilpikonna <admin@moecraft.cc>
2019-10-26 15:16:18 -0400

    [F] Should be line width

--- src/renderer.py
+++ src/renderer.py
@@ -9,9 +9,9 @@ grid_start_y = 50
 grid_length_x = 900
 grid_length_y = 900
 grid_line_color = (95, 95, 95)  # 5f5f5f
-grid_line_length = 1
+grid_line_width = 1
 border_line_color = (150, 150, 150)
-border_line_length = 2
+border_line_width = 2
 block_width = 9
 
 
@@ -37,19 +37,19 @@ class Renderer:
         for x in range(grid_start_x, grid_start_x + grid_length_x):
 
             # Every gap between blocks
-            if x % (block_width + grid_line_length) == 0:
+            if x % (block_width + grid_line_width) == 0:
 
                 # Draw a line
-                self.pygame.draw.rect(self.window, grid_line_color, (x + block_width, grid_start_y, grid_line_length, grid_length_x))
+                self.pygame.draw.rect(self.window, grid_line_color, (x + block_width, grid_start_y, grid_line_width, grid_length_x))
 
         # Loop through all y values in the grid
         for y in range(grid_start_y, grid_start_y + grid_length_y):
 
             # Every gap between blocks
-            if y % (block_width + grid_line_length) == 0:
+            if y % (block_width + grid_line_width) == 0:
 
                 # Draw a line
-                self.pygame.draw.rect(self.window, grid_line_color, (grid_start_x, y + block_width, grid_length_y, grid_line_length))
+                self.pygame.draw.rect(self.window, grid_line_color, (grid_start_x, y + block_width, grid_length_y, grid_line_width))
 
 
 
