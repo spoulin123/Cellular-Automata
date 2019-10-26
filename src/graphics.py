1741ee4369e38ebf975e5525a3b599c21d13394f
Hykilpikonna <admin@moecraft.cc>
2019-10-26 15:59:37 -0400

    [+] Create world in graphics.py

--- src/graphics.py
+++ src/graphics.py
@@ -1,5 +1,8 @@
 import pygame
 from renderer import Renderer
+from rules import Rule, Condition
+from world import World
+from presets import ConwayPatterns
 
 # Initialize pygame
 pygame.init()
@@ -9,6 +12,17 @@ renderer = Renderer(pygame)
 
 # Render initial stuff
 renderer.drawGrid()
+
+# Define rules
+conway_rules = [Rule(1, [Condition(1, "LESS", 2)], 0),
+                Rule(1, [Condition(1, "GREATER_EQUAL", 2), Condition(1, "LESS", 4)], 1),
+                Rule(1, [Condition(1, "GREATER_EQUAL", 4)], 0),
+                Rule(0, [Condition(1, "EQUAL", 3)], 1)]
+
+# Create world
+world = World("Test", 2000, 2000, 2, conway_rules, {0: " ", 1: "#"}, {})
+world.spawn_points(ConwayPatterns.r_pentomino((10, 10)), 1)
+print(world.active_points)
 
 # Frame operations
 running = True
