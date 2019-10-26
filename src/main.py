c21c9c35efcd81a36593eda74aa2deed45bef72e
Hykilpikonna <admin@moecraft.cc>
2019-10-26 13:51:32 -0400

    [M] Move sources into src dir

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import time
 
-from presets import ConwayPatterns
-from rules import Rule, Condition
-from world import World
+from src.presets import ConwayPatterns
+from src.rules import Rule, Condition
+from src.world import World
 
 # Define rules
 rule1 = Rule(1, [Condition(1, "LESS", 2)], 0)
